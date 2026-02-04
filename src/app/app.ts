8ce55933bc0e485e64ca73f05f2ea1c82e678a17
Al-AminWeb <mdalaminweb.1@gmail.com>
2026-02-04 22:16:48 +0600

    Add error handling middleware for 404 and 500 responses

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, {type NextFunction, type Request, type Response} from "express";
 import type {Application} from "express";
 import {prisma} from "../shared/prisma.js";
 import {toNodeHandler} from "better-auth/node";
@@ -10,6 +10,15 @@ app.all('/api/v1/auth/{*any}', toNodeHandler(auth));
 
 app.get("/", async (req, res) => {
     res.send("Hello World!");
+})
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    console.log(err)
+    res.status(500).json({message: err.message, error: 'Internal Server Error'});
+})
+
+app.use((req, res) => {
+    res.status(404).json({message: 'Not Found'});
 })
 
 export default app;
